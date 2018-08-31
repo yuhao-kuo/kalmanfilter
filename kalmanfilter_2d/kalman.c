950411535e757889803b9678e3b0b94781766ee4
yuhao.kuo <st40201@gmail.com>
2018-09-01 01:51:32 +0800

    [add] add 2 sensors kalman filter
    add 2 sensors kalman filter.
    this commit is 'bata'
    
    add files;
    * kalmanfilter_2d/kalman.c
    * kalmanfilter_2d/kalman.h

---
+++ kalmanfilter_2d/kalman.c
@@ -0,0 +1,59 @@
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "kalman.h"
+
+bool kalmanSetP(Kalman_t *obj, uint32_t p)
+{
+    obj->P = p;
+    return true;
+}
+
+bool kalmanSetQ(Kalman_t *obj, uint32_t q)
+{
+    obj->Q = q;
+    return true;
+}
+
+bool kalmanSetR(Kalman_t *obj, uint32_t r1, uint32_t r2)
+{
+    obj->R[0] = r1;
+    obj->R[1] = r2;
+    return true;
+}
+
+bool kalmanSetNewX(Kalman_t *obj, uint32_t x1, uint32_t x2)
+{
+    obj->X[0] = (x1 + x2) >> 1;
+    return true;
+}
+
+uint16_t kalmanfilter(Kalman_t *obj, uint16_t sen1, uint16_t sen2)
+{
+    uint32_t var = 0;
+    uint32_t var1 = sen1 * 100000;
+    uint32_t var2 = sen2 * 100000;
+
+    uint32_t a, d, e, p, g1, g2;
+
+    // Predict
+    obj->X[1] = obj->X[0];
+    obj->P += obj->Q;
+    p = obj->P;
+
+    // Update
+    a = p + obj->R[0];
+    d = p + obj->R[1];
+    e = p / (a * d - p * p);
+
+    g1 = e * d - e * p;
+    g2 = e * a - e * p;
+
+    obj->P = (1 - (g1 + g2)) * p;
+
+    var = obj->X[1] + g1 * (var1 - obj->X[1]) + g2 * (var2 - obj->X[1]);
+    obj->X[0] = var;
+
+    return var / 100000;
+}
+
---
+++ kalmanfilter_2d/kalman.h
@@ -0,0 +1,34 @@
+
+
+
+#ifndef __KALMAN_H__
+#define __KALMAN_H__
+
+#ifdef __cpluplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    /* Kalman filter variables */
+    uint32_t P;
+    uint32_t Q;
+    uint32_t X[2];
+    uint32_t R[2];
+} Kalman_t;
+
+extern void kalmanInit(Kalman_t *obj);
+
+bool kalmanSetP(Kalman_t *obj, uint32_t p);
+bool kalmanSetQ(Kalman_t *obj, uint32_t q);
+bool kalmanSetR(Kalman_t *obj, uint32_t r1, uint32_t r2);
+bool kalmanSetNewX(Kalman_t *obj, uint32_t x1, uint32_t x2);
+uint16_t kalmanfilter(Kalman_t *obj, uint16_t sen1, uint16_t sen2);
+
+#ifdef __cpluplus
+}
+#endif
+
+#endif
